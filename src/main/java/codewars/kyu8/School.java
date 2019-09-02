baf648f9bc85fabcb68548efc5e066c141a43892
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-09-02 17:59:39 +0200

    https://www.codewars.com/kata/563e320cee5dddcf77000158

---
+++ src/main/java/codewars/kyu8/School.java
@@ -0,0 +1,13 @@
+package codewars.kyu8;
+
+import java.util.Arrays;
+
+public class School{
+
+ 	public static int getAverage(int[] marks){
+		return (int) Arrays.stream(marks)
+				.average()
+				.getAsDouble();
+	}
+
+}
