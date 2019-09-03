ff0497f5eff5c2ec2fef1c6eb9a705f2a60723ac
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-09-04 01:08:13 +0200

    https://www.codewars.com/kata/577bd026df78c19bca0002c0

---
+++ src/main/java/codewars/kyu8/Correct.java
@@ -0,0 +1,9 @@
+package codewars.kyu8;
+
+public class Correct {
+  public static String correct(String string) {
+    return string.replace('5','S')
+                  .replace('0','O')
+                  .replace('1','I');
+  }
+}
