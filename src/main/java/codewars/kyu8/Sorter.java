b27574abf9e2d64361d2d5dff193747d33f68192
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-09-05 02:39:36 +0200

    Renamed file

--- src/main/java/codewars/kyu8/Sorter.java
+++ src/main/java/codewars/kyu8/Sorter.java
@@ -1,10 +1,8 @@
 package codewars.kyu8;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
-class sorter {
+class Sorter {
     public static List<String> sort(List<String> textbooks) {
         textbooks.sort(String::compareToIgnoreCase);
         return textbooks;
