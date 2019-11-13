52db5a2278ef5547d5b8d240e59657c14ca21909
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-11-13 16:06:05 +0100

    https://www.codewars.com/kata/58f8a3a27a5c28d92e000144

---
+++ src/main/java/codewars/kyu8/FirstNonConsecutive.java
@@ -0,0 +1,12 @@
+package codewars.kyu8;
+
+class FirstNonConsecutive {
+    static Integer find(final int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i]+1 != array[i+1]) {
+                return array[i+1];
+            }
+        }
+        return null;
+    }
+}
