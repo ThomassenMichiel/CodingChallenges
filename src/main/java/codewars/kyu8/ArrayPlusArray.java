97d6ecd6947c9aa227d665c9c66029527ff9e46d
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-11-14 17:44:14 +0100

    https://www.codewars.com/kata/5a2be17aee1aaefe2a000151

---
+++ src/main/java/codewars/kyu8/ArrayPlusArray.java
@@ -0,0 +1,11 @@
+package codewars.kyu8;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ArrayPlusArray {
+  public static int arrayPlusArray(int[] arr1, int[] arr2) {
+    return IntStream.concat(Arrays.stream(arr1), Arrays.stream(arr2))
+            .sum();
+  }
+}
