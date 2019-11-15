c6dbf3169509f38d37a10c4f26e154521f52607d
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-11-15 20:50:49 +0100

    https://www.codewars.com/kata/525c1a07bb6dda6944000031/

---
+++ src/main/java/codewars/kyu8/KataExampleTwist.java
@@ -0,0 +1,11 @@
+package codewars.kyu8;
+
+import java.util.stream.IntStream;
+
+public class KataExampleTwist {
+    public static String[] kataExampleTwist() {
+        return IntStream.range(0,1000)
+                .mapToObj(i -> "codewars")
+                .toArray(String[]::new);
+    }
+}
