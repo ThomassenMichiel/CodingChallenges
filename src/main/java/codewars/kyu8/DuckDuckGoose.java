0ec5a9b97ed5e257d26f84ccad933ee037df76c4
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-11-16 01:47:02 +0100

    https://www.codewars.com/kata/582e0e592029ea10530009ce/

---
+++ src/main/java/codewars/kyu8/DuckDuckGoose.java
@@ -0,0 +1,13 @@
+package codewars.kyu8;
+
+public class DuckDuckGoose {
+  /**
+   * Code won't compile because I have no idea where the Player class is located?
+   * Code is commented to make the rest of the project compilable
+   */
+  /*public static String duckDuckGoose(Player[] players, int goose) {
+    goose %= players.length;
+    goose = goose == 0 ? players.length: goose--;
+    return players[goose - 1].name;
+  }*/
+}
