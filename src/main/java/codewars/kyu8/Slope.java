35a8cc2c2244361dd525e652e66f3a9d8e1f3bbd
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-11-16 18:18:45 +0100

    https://www.codewars.com/kata/55a75e2d0803fea18f00009d/

---
+++ src/main/java/codewars/kyu8/Slope.java
@@ -0,0 +1,11 @@
+package codewars.kyu8;
+
+public class Slope {
+    public String slope(int[] points) {
+        try {
+            return String.valueOf((points[3] - points[1]) / (points[2] - points[0]));
+        } catch (ArithmeticException ae) {
+            return "undefined";
+        }
+    }
+}
