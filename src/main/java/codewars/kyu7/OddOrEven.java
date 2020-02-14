00a89d98b4d2a6d5a3374ec5888c35113c5b16b7
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-02-14 14:52:29 +0100

    OddOrEven

---
+++ src/main/java/codewars/kyu7/OddOrEven.java
@@ -0,0 +1,9 @@
+package codewars.kyu7;
+
+import java.util.Arrays;
+
+public class OddOrEven {
+    public static String oddOrEven(int[] array) {
+      return Arrays.stream(array).sum() % 2 == 0 ? "even" : "odd";
+    }
+}
