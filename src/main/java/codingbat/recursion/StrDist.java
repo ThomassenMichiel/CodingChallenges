f7a83ca06e3e30eb66352ff8ecabe2a2183b99f0
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-03-11 14:40:54 +0100

    StrDist working and passing tests

--- src/main/java/codingbat/recursion/StrDist.java
+++ src/main/java/codingbat/recursion/StrDist.java
@@ -2,14 +2,14 @@ package codingbat.recursion;
 
 public class StrDist {
     public int strDist(String str, String sub) {
-        if (str.startsWith(sub) && sub.endsWith(sub)) {
+        if (str.startsWith(sub) && str.endsWith(sub)) {
             return str.length();
         }
         if (!str.startsWith(sub)) {
             return strDist(str.substring(1),sub);
         }
         if (!str.endsWith(sub)) {
-            return strDist(str.substring(str.length()-1),sub);
+            return strDist(str.substring(0,str.length()-1),sub);
         }
         return 0;
     }
