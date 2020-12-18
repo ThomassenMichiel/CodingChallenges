173085823ca1a47432b0abe80b6585701cb06b1c
SnOwBunZz <bunzzbunzz@gmail.com>
2020-12-18 12:03:49 +0100

    Goals

--- src/main/java/codewars/kyu8/Goals.java
+++ src/main/java/codewars/kyu8/Goals.java
@@ -1,6 +1,13 @@
 package codewars.kyu8;
 
 public class Goals {
+    
+    public static int laLigaGoals = 43;
+    public static int championsLeagueGoals = 10;
+    public static int copaDelReyGoals = 5;
+    
+    public static int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+    
     public static int goals(int a, int b, int c) {
         return a + b + c;
     }
