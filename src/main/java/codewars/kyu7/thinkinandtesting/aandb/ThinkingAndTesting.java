a18f454d353241b50990b9bb2940902021a26e15
SnOwBunZz <bunzzbunzz@gmail.com>
2021-04-23 14:57:46 +0200

    Thinking and Testing

---
+++ src/main/java/codewars/kyu7/thinkinandtesting/aandb/ThinkingAndTesting.java
@@ -0,0 +1,10 @@
+package codewars.kyu7.thinkinandtesting.aandb;
+
+/**
+ * url: https://www.codewars.com/kata/56d904db9963e9cf5000037d
+ */
+public class ThinkingAndTesting {
+    public static int testAB(int a, int b) {
+        return a | b;
+    }
+}
