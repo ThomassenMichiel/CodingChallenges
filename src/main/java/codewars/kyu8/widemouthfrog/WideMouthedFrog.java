93a19c84ab98fafa0f68ca9ac6b8970910397fe8
Michiel Thomassen <bunzzbunzz@gmail.com>
2021-07-30 23:36:41 +0200

    WideMouthedFrog

---
+++ src/main/java/codewars/kyu8/widemouthfrog/WideMouthedFrog.java
@@ -0,0 +1,7 @@
+package codewars.kyu8.widemouthfrog;
+
+public class WideMouthedFrog {
+    public static String mouthSize(String animal) {
+        return "alligator".equalsIgnoreCase(animal) ? "small" : "wide";
+    }
+}
---
+++ src/test/java/codewars/kyu8/widemouthfrog/WideMouthedFrogTest.java
@@ -0,0 +1,17 @@
+package codewars.kyu8.widemouthfrog;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.runners.JUnit4;
+
+public class WideMouthedFrogTest {
+
+    @Test
+    public void fixedTests() {
+        assertEquals("wide", WideMouthedFrog.mouthSize("toucan"));
+        assertEquals("wide", WideMouthedFrog.mouthSize("ant bear"));
+        assertEquals("small", WideMouthedFrog.mouthSize("alligator"));
+    }
+}
