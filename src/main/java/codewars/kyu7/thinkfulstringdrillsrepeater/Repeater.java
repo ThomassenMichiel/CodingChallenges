3507a2a068f14f0652219da1818fff66a8267ffd
SnOwBunZz <bunzzbunzz@gmail.com>
2021-08-19 12:18:38 +0200

    Thinkful - String Drills: Repeater

---
+++ src/main/java/codewars/kyu7/thinkfulstringdrillsrepeater/Repeater.java
@@ -0,0 +1,10 @@
+package codewars.kyu7.thinkfulstringdrillsrepeater;
+
+/**
+ * url: https://www.codewars.com/kata/585a1a227cb58d8d740001c3
+ */
+public class Repeater{
+  public static String repeat(String string,long n){
+      return string.repeat((int) n);
+  }
+}
