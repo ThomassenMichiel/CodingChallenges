56951a0567bc7b2cafdc5d8e970694ceb71dd8b1
SnOwBunZz <bunzzbunzz@gmail.com>
2021-09-17 14:54:12 +0200

    Waiting room

---
+++ src/main/java/codewars/kyu7/waitingroom/Kata.java
@@ -0,0 +1,10 @@
+package codewars.kyu7.waitingroom;
+
+/**
+ * url: https://www.codewars.com/kata/542f0c36d002f8cd8a0005e5
+ */
+public class Kata {
+    public static int lastChair(int n) {
+        return n - 1;
+    }
+}
---
+++ src/test/java/codewars/kyu7/waitingroom/ExampleKataTests.java
@@ -0,0 +1,12 @@
+package codewars.kyu7.waitingroom;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class ExampleKataTests {
+    @Test
+    public void tests() {
+        assertEquals(9, Kata.lastChair(10));
+    }
+}
