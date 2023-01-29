83fc2d58635ff4317fa1eff53e0eb08ec8ad5833
Michiel Thomassen <bunzzbunzz@gmail.com>
2023-01-29 16:59:21 +0100

    Return the day

---
+++ src/main/java/codewars/kyu8/returntheday/DayOfWeek.java
@@ -0,0 +1,17 @@
+package codewars.kyu8.returntheday;
+
+public class DayOfWeek {
+    public static String getDay(int n) {
+        String day = "Wrong, please enter a number between 1 and 7";
+        switch (n) {
+            case 1: day = "Sunday"; break;
+            case 2: day = "Monday"; break;
+            case 3: day = "Tuesday"; break;
+            case 4: day = "Wednesday"; break;
+            case 5: day = "Thursday"; break;
+            case 6: day = "Friday"; break;
+            case 7: day = "Saturday"; break;
+        }
+        return day;
+    }
+}
---
+++ src/test/java/codewars/kyu8/returntheday/DayOfWeekTest.java
@@ -0,0 +1,19 @@
+package codewars.kyu8.returntheday;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// TODO: Replace examples and use TDD by writing your own tests
+
+class SolutionTest {
+    @Test
+    void testSomething() {
+        assertEquals("Sunday", DayOfWeek.getDay(1));
+        assertEquals("Monday", DayOfWeek.getDay(2));
+        assertEquals("Tuesday", DayOfWeek.getDay(3));
+        assertEquals("Wrong, please enter a number between 1 and 7", DayOfWeek.getDay(8));
+        assertEquals("Wrong, please enter a number between 1 and 7", DayOfWeek.getDay(20));
+    }
+}
+
