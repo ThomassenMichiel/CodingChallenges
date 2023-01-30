1580927444de59717586bcd2036487e6f65e1816
Michiel Thomassen <bunzzbunzz@gmail.com>
2023-01-30 01:28:42 +0100

    Binary to Decimal

---
+++ src/main/java/codewars/kyu8/bintodecimal/Converter.java
@@ -0,0 +1,7 @@
+package codewars.kyu8.bintodecimal;
+
+public class Converter {
+    public static int binToDecimal(String inp) {
+        return Integer.parseInt(inp, 2);
+    }
+}
---
+++ src/test/java/codewars/kyu8/bintodecimal/ConverterTest.java
@@ -0,0 +1,17 @@
+package codewars.kyu8.bintodecimal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.runners.JUnit4;
+
+public class ConverterTest {
+
+    @Test
+    public void testHoopCount() {
+        assertEquals(1, Converter.binToDecimal("1"));
+        assertEquals(0, Converter.binToDecimal("0"));
+        assertEquals(73, Converter.binToDecimal("1001001"));
+    }
+}
