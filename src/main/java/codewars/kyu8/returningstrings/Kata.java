0a5cd287e3f05426280c9d10d859002f09ed86c1
Michiel Thomassen <bunzzbunzz@gmail.com>
2023-01-30 01:15:13 +0100

    Returning Strings

---
+++ src/main/java/codewars/kyu8/returningstrings/Kata.java
@@ -0,0 +1,7 @@
+package codewars.kyu8.returningstrings;
+
+public class Kata {
+    public static String greet(String name) {
+        return "Hello, " + name + " how are you doing today?";
+    }
+}
---
+++ src/test/java/codewars/kyu8/returningstrings/SolutionTest.java
@@ -0,0 +1,15 @@
+package codewars.kyu8.returningstrings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void testSomething() {
+        assertEquals("Hello, Ryan how are you doing today?", Kata.greet("Ryan"));
+    }
+}
+
