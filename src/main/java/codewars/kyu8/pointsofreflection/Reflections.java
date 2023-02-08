3403e34e673e2ea188f2d2f47488eda2b9c4e34e
Michiel Thomassen <mich.thomassen@gmail.com>
2023-02-08 15:16:10 +0100

    Reflection points

---
+++ src/main/java/codewars/kyu8/pointsofreflection/Reflections.java
@@ -0,0 +1,9 @@
+package codewars.kyu8.pointsofreflection;
+
+public class Reflections {
+
+    public static int[] reflectPoint(int[] p, int[] q) {
+        return new int[]{2*q[0]-p[0], 2*q[1]-p[1]};
+    }
+
+}
